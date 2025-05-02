585e12479f8e80dc6a5b1396cd0dcd5801ced1d9
saranshverma21345 <82048395+saranshverma21345@users.noreply.github.com>
2025-05-02 17:16:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,47 +7,48 @@ const wikip = require('wiki-infobox-parser');
 app.set("view engine", 'ejs');
 
 //routes
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/index', (req,response) =>{
-    let url = "https://en.wikipedia.org/w/api.php"
-    let params = {
+app.get('/index', (req, response) => {
+    if (!req.query.person) {
+        return response.status(400).send('Missing search parameter');
+    }
+
+    let url = "https://en.wikipedia.org/w/api.php";
+    const params = new URLSearchParams({
         action: "opensearch",
         search: req.query.person,
         limit: "1",
         namespace: "0",
         format: "json"
-    }
+    }).toString();
 
-    url = url + "?"
-    Object.keys(params).forEach( (key) => {
-        url += '&' + key + '=' + params[key]; 
-    });
+    url = `${url}?${params}`;
 
     //get wikip search string
-    request(url,(err,res, body) =>{
-        if(err) {
-            response.redirect('404');
+    request(url, (err, res, body) => {
+        if (err) {
+            return response.status(500).send('Error fetching data');
         }
-            result = JSON.parse(body);
-            x = result[3][0];
-            x = x.substring(30, x.length); 
-            //get wikip json
-            wikip(x , (err, final) => {
-                if (err){
-                    response.redirect('404');
-                }
-                else{
-                    const answers = final;
-                    response.send(answers);
-                }
-            });
-    });
+        const result = JSON.parse(body);
+        let x = result[3][0];
+        x = x.substring(30, x.length);
 
-    
+        //get wikip json
+        wikip(x, (err, final) => {
+            if (err) {
+                return response.status(500).send('Error parsing Wikipedia data');
+            } else {
+                const answers = final;
+                response.send(answers);
+            }
+        });
+    });
 });
 
 //port
-app.listen(3000, console.log("Listening at port 3000..."))
+app.listen(3000, '0.0.0.0', () => {
+    console.log("Listening at port 3000...");
+});
